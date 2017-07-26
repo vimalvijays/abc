ae71a5ba20f3d65bef8b3f1d08056b2f35828466
vimalvijays <vimal.m@hashworks.co>
2017-07-26 18:25:31 +0530

    Initial Commit

---
+++ src/test/java/com/packs/packs/PacksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.packs.packs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PacksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
